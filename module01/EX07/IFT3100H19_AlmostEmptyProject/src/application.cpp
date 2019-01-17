89d189f42b64865243c6f51a0c8cf9ea3dd1081e
philvoyer <philippe.voyer@ift.ulaval.ca>
2019-01-17 14:05:51 -0500

    publication exemple 1.7 (AlmostEmptyProject)

---
+++ module01/EX07/IFT3100H19_AlmostEmptyProject/src/application.cpp
@@ -0,0 +1,32 @@
+// IFT3100H19_AlmostEmptyProject/application.cpp
+// Classe principale de l'application.
+
+#include "application.h"
+
+// fonction d'initialisation de l'application
+void Application::setup()
+{
+  ofSetWindowTitle("projet (presque) vide");
+
+  renderer.setup();
+
+  ofLog() << "<app::setup>";
+}
+
+// fonction de mise à jour de la logique de l'application
+void Application::update()
+{
+  renderer.update();
+}
+
+// fonction de mise à jour du rendu de la fenêtre d'affichage de l'application
+void Application::draw()
+{
+  renderer.draw();
+}
+
+// fonction appelée juste avant de quitter l'application
+void Application::exit()
+{
+  ofLog() << "<app::exit>";
+}
