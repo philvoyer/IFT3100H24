b7c7dbd2eeb843ebc3e3f754359e25e7c24f8382
philvoyer <philippe.voyer@ift.ulaval.ca>
2019-01-30 19:02:16 -0500

    publication exemple 2.2 (DrawCursor)

---
+++ module02/EX02/IFT3100H19_DrawCursor/src/main.cpp
@@ -0,0 +1,11 @@
+// IFT3100H19_DrawCursor/main.cpp
+// Dessiner un curseur en fonction des états et des événements d'un périphérique de pointage.
+
+#include "ofMain.h"
+#include "application.h"
+
+int main( )
+{
+  ofSetupOpenGL(512, 512, OF_WINDOW);
+  ofRunApp(new Application());
+}
---
+++ module02/EX02/IFT3100H19_DrawCursor/src/renderer.h
@@ -0,0 +1,24 @@
+// IFT3100H19_DrawCursor/renderer.h
+// Classe responsable du rendu de l'application.
+
+#pragma once
+
+#include "ofMain.h"
+
+class Renderer
+{
+public:
+
+  int mouse_press_x;
+  int mouse_press_y;
+
+  int mouse_current_x;
+  int mouse_current_y;
+
+  bool is_mouse_button_pressed;
+
+  void setup();
+  void draw();
+
+  void draw_cursor(float x, float y) const;
+};
