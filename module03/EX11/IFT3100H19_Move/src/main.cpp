39338a6646cf258d41dc6a1f5bd6d9e2f04838e8
philvoyer <philippe.voyer@ift.ulaval.ca>
2019-02-07 08:36:14 -0500

    publication exemple 3.11 (Move)

---
+++ module03/EX11/IFT3100H19_Move/src/main.cpp
@@ -0,0 +1,11 @@
+// IFT3100H19_Move/main.cpp
+// Mouvement dans l'espace sur le plan XZ à partir de l'état courant des flèches du clavier.
+
+#include "ofMain.h"
+#include "application.h"
+
+int main()
+{
+  ofSetupOpenGL(512, 512, OF_WINDOW);
+  ofRunApp(new Application());
+}
