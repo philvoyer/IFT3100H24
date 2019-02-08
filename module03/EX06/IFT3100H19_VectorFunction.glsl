755622220acb7646e8c0bc4c828dc54b472a68e0
philvoyer <philippe.voyer@ift.ulaval.ca>
2019-02-07 19:04:26 -0500

    publication exemple 3.6 (VectorFunction)

--- module03/EX06/IFT3100H19_VectorFunction.glsl
+++ module03/EX06/IFT3100H19_VectorFunction.glsl
@@ -14,7 +14,7 @@ vec3 v = normalize(vec3(1.0, 2.0, 3.0));
 // 2. magnitude d'un vecteur
 float f = length(vec3(1.0, 2.0, 3.0));
 
-// 3. calculer le produit croisé entre deux axes
+// 3. calculer le produit vectoriel entre deux axes
 vec3 axis_z = cross(vec3(1.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0));
 
 // 4. calculer la réflexion diffuse d'un matériau
@@ -24,7 +24,11 @@ float n_dot_l = dot(normal, light);
 vec3 r = reflect(-light, normal);
 
 // 6. calculer la réflexion spéculaire d'un matériau
-float r_dot_v = pow(dot(r, view) p);
+float r_dot_v = pow(dot(r, v) p);
 
-// 7. calculer une combinaison de deux types de réflexion d'un matériau
-float color = mix(n_dot_l, r_dot_v, 0.5);
+// 7. calculer la couleur de la surface d'un matériau (modèle de Phong)
+vec3 color_ambient  = mix(vec3(0.0), vec3(1.0), 0.1); // gris foncé
+vec3 color_diffuse  = mix(vec3(0.0), vec3(1.0), 0.5); // gris
+vec3 color_specular = mix(vec3(0.0), vec3(1.0), 1.0); // blanc
+
+vec3 color_surface = color_ambient + n_dot_l * color_diffuse + r_dot_v * color_specular;
