888ff45ce7c7eec196d07829b3b66d998ae222b7
philvoyer <philippe.voyer@ift.ulaval.ca>
2019-02-13 19:57:15 -0500

    publication exemple 4.10 (TriangleSoup)

---
+++ module04/EX10/IFT3100H19_TriangleSoup/src/renderer.cpp
@@ -0,0 +1,203 @@
+// IFT3100H19_TriangleSoup/renderer.cpp
+// Classe responsable du rendu de l'application.
+
+#include "renderer.h"
+
+void Renderer::setup()
+{
+  // paramètres du programme
+  triangle_count = 2500;
+  triangle_radius = 16.0f;
+  soup_proportion = 0.4f;
+  bowl_or_ball = true;
+  speed = 100;
+
+  // paramètres de rendu
+  ofSetFrameRate(60);
+  ofEnableDepthTest();
+
+  // initialisation des variables
+  offset_x = 0;
+  offset_y = 0;
+  offset_z = 0;
+
+  delta_x = speed;
+  delta_y = speed;
+  delta_z = speed;
+
+  triangle_buffer_size = triangle_count;
+  triangle_buffer_head = 0;
+
+  // allocation d'un espace mémoire suffisament grand pour contenir les données de l'ensemble des triangles de la soupe
+  soup = (Triangle*) std::malloc(triangle_buffer_size * sizeof(Triangle));
+
+  reset();
+}
+
+// fonction qui initialise la scène
+void Renderer::reset()
+{
+  // calculer les coordonnées du centre du framebuffer
+  center_x = ofGetWidth() / 2.0f;
+  center_y = ofGetHeight() / 2.0f;
+
+  // déterminer le rayon de la soupe en fonction des dimensions de la fenêtre
+  soup_radius = std::min(ofGetWidth(), ofGetHeight()) * soup_proportion;
+
+  // distribuer les triangles dans l'espace de la scène
+  dispatch_random_triangle(triangle_count, soup_radius);
+
+  ofLog() << "<reset>";
+}
+
+// fonction qui distribue les triangles dans un hémisphère
+void Renderer::dispatch_random_triangle(int count, float range)
+{
+  // variable temporaire
+  float scale;
+
+  // validations
+  if (count <= 0 || range <= 0.0f || count > triangle_buffer_size)
+    return;
+
+  // calculer la valeur de la moitié du diamètre du cube
+  float halfRange = range / 2.0f;
+
+  // configurer le nombre de triangles
+  triangle_buffer_head = count;
+
+  for (int index = 0; index < triangle_buffer_head; ++index)
+  {
+    // déterminer une position au hasard
+    vector_origin.x = ofRandom(-1.0f, 1.0f);
+    vector_origin.y = ofRandom(-1.0f, 1.0f);
+    vector_origin.z = ofRandom(-1.0f, 1.0f);
+
+    // normaliser la position
+    vector_origin.normalize();
+
+    // ramener la position dans une hémisphère
+    if (bowl_or_ball)
+      vector_origin.z = std::abs(vector_origin.z) * -1.0f;
+
+    // proportion de la soupe
+    vector_origin *= soup_radius;
+
+    // déterminer des valeurs de position aléatoires pour les trois sommets du triangle
+    vector_position1.x = vector_origin.x + ofRandom(-triangle_radius, triangle_radius);
+    vector_position1.y = vector_origin.y + ofRandom(-triangle_radius, triangle_radius);
+    vector_position1.z = vector_origin.z + ofRandom(-triangle_radius, triangle_radius);
+
+    vector_position2.x = vector_origin.x + ofRandom(-triangle_radius, triangle_radius);
+    vector_position2.y = vector_origin.y + ofRandom(-triangle_radius, triangle_radius);
+    vector_position2.z = vector_origin.z + ofRandom(-triangle_radius, triangle_radius);
+
+    vector_position3.x = vector_origin.x + ofRandom(-triangle_radius, triangle_radius);
+    vector_position3.y = vector_origin.y + ofRandom(-triangle_radius, triangle_radius);
+    vector_position3.z = vector_origin.z + ofRandom(-triangle_radius, triangle_radius);
+
+    // déterminer une couleur RGB au hasard
+    vector_color.r = ofRandom(0, 255);
+    vector_color.g = ofRandom(0, 255);
+    vector_color.b = ofRandom(0, 255);
+    vector_color.a = 255;
+
+    // configurer les attributs de transformation du triangle
+    soup[index].position1[0] = vector_position1.x;
+    soup[index].position1[1] = vector_position1.y;
+    soup[index].position1[2] = vector_position1.z;
+
+    soup[index].position2[0] = vector_position2.x;
+    soup[index].position2[1] = vector_position2.y;
+    soup[index].position2[2] = vector_position2.z;
+
+    soup[index].position3[0] = vector_position3.x;
+    soup[index].position3[1] = vector_position3.y;
+    soup[index].position3[2] = vector_position3.z;
+
+    // configurer les attributs de couleur du triangle
+    soup[index].color[0] = vector_color.r;
+    soup[index].color[1] = vector_color.g;
+    soup[index].color[2] = vector_color.b;
+    soup[index].color[3] = vector_color.a;
+  }
+}
+
+void Renderer::draw()
+{
+  ofBackgroundGradient(ofColor(191), ofColor(63));
+
+  ofPushMatrix();
+
+  ofTranslate(center_x + offset_x, center_y, offset_z);
+
+  // dessiner la soupe de triangles
+  draw_soup();
+
+  draw_locator(3.0f);
+
+  ofPopMatrix();
+}
+
+// fonction de rendu de la soupe aux triangles
+void Renderer::draw_soup()
+{
+  ofPushMatrix();
+
+  // rotation en Y de la scène
+  ofRotateDeg(offset_y, 0.0f, 1.0f, 0.0f);
+
+  // légère rotation en Z de la soupe
+  ofRotateDeg(ofGetFrameNum() * 0.1f, 0.0f, 0.0f, 1.0f);
+
+  // dessiner les triangles
+  for (int index = 0; index < triangle_buffer_head; ++index)
+    draw_triangle(index);
+
+  ofPopMatrix();
+}
+
+// fonction qui dessine un des triangles de la soupe
+void Renderer::draw_triangle(int index)
+{
+  ofFill();
+
+  ofSetColor(
+    soup[index].color[0],
+    soup[index].color[1],
+    soup[index].color[2],
+    soup[index].color[3]);
+
+  vector_position1.x = soup[index].position1[0];
+  vector_position1.y = soup[index].position1[1];
+  vector_position1.z = soup[index].position1[2];
+
+  vector_position2.x = soup[index].position2[0];
+  vector_position2.y = soup[index].position2[1];
+  vector_position2.z = soup[index].position2[2];
+
+  vector_position3.x = soup[index].position3[0];
+  vector_position3.y = soup[index].position3[1];
+  vector_position3.z = soup[index].position3[2];
+
+  ofDrawTriangle(
+    vector_position1,
+    vector_position2,
+    vector_position3);
+}
+
+void Renderer::draw_locator(float scale)
+{
+  ofFill();
+  ofSetColor(191);
+  ofPushMatrix();
+  ofScale(scale, scale);
+  node.setPosition(0.0f, 0.0f, 0.0f);
+  node.draw();
+  ofPopMatrix();
+}
+
+Renderer::~Renderer()
+{
+  std::free(soup);
+}
---
+++ module04/EX10/IFT3100H19_TriangleSoup/src/renderer.h
@@ -0,0 +1,77 @@
+// IFT3100H19_TriangleSoup/renderer.h
+// Classe responsable du rendu de l'application.
+
+#pragma once
+
+#include "ofMain.h"
+
+// structure d'un triangle
+struct Triangle
+{
+  GLfloat position1[3]; // 3 * 4 = 12 octets
+  GLfloat position2[3]; // 3 * 4 = 12 octets
+  GLfloat position3[3]; // 3 * 4 = 12 octets
+  GLubyte color[4];     // 4 * 1 = 4  octets
+};                      //       = 40 octets
+
+class Renderer
+{
+public:
+
+  Triangle* soup;
+
+  ofVec3f vector_origin;
+
+  ofVec3f vector_position1;
+  ofVec3f vector_position2;
+  ofVec3f vector_position3;
+
+  ofColor vector_color;
+
+  ofLight light;
+
+  ofNode node;
+
+  float center_x;
+  float center_y;
+
+  float speed;
+
+  float offset_x;
+  float offset_y;
+  float offset_z;
+
+  float delta_x;
+  float delta_y;
+  float delta_z;
+
+  float soup_radius;
+  float soup_proportion;
+
+  float triangle_radius;
+
+  int triangle_count;
+
+  int triangle_buffer_size;
+  int triangle_buffer_head;
+
+  bool bowl_or_ball;
+
+  void setup();
+
+  void dispatch_random_triangle(int count, float range);
+
+  void reset();
+
+  void draw();
+
+  void draw_scene();
+
+  void draw_soup();
+
+  void draw_triangle(int index);
+
+  void draw_locator(float scale);
+
+  ~Renderer();
+};
