51812a68b9b2aeece9da99078fb0ba81e831fcfd
philvoyer <philippe.voyer@ift.ulaval.ca>
2019-02-14 08:16:40 -0500

    publication exemple 4.11 (Teapot)

---
+++ module04/EX11/IFT3100H19_Teapot/src/application.cpp
@@ -0,0 +1,133 @@
+// IFT3100H19_Teapot/application.cpp
+// Classe principale de l'application.
+
+#include "application.h"
+
+void Application::setup()
+{
+  ofLog() << "<app::setup>";
+
+  ofSetWindowTitle("teapot (↑ ↓ ← → 1 2 3 w e r f)");
+
+  is_key_press_up = false;
+  is_key_press_down = false;
+  is_key_press_left = false;
+  is_key_press_right = false;
+
+  renderer.setup();
+}
+
+void Application::update()
+{
+  time_current = ofGetElapsedTimef();
+  time_elapsed = time_current - time_last;
+  time_last = time_current;
+
+  if (is_key_press_up)
+    renderer.offset_z += renderer.delta_z * time_elapsed;
+  if (is_key_press_down)
+    renderer.offset_z -= renderer.delta_z * time_elapsed;
+  if (is_key_press_left)
+    renderer.offset_x += renderer.delta_x * time_elapsed;
+  if (is_key_press_right)
+    renderer.offset_x -= renderer.delta_x * time_elapsed;
+
+  renderer.update();
+}
+
+void Application::draw()
+{
+  renderer.draw();
+}
+
+void Application::mouseReleased(int x, int y, int button)
+{
+  renderer.reset();
+}
+
+void Application::keyPressed(int key)
+{
+  switch (key)
+  {
+    case OF_KEY_LEFT: // key ←
+      is_key_press_left = true;
+      break;
+
+    case OF_KEY_UP: // key ↑
+      is_key_press_up = true;
+      break;
+
+    case OF_KEY_RIGHT: // key →
+      is_key_press_right = true;
+      break;
+
+    case OF_KEY_DOWN: // key ↓
+      is_key_press_down = true;
+      break;
+
+    default:
+      break;
+  }
+}
+
+void Application::keyReleased(int key)
+{
+  switch (key)
+  {
+    case 49:  // touche 1
+      renderer.mesh_render_mode = MeshRenderMode::fill;
+      ofLog() << "<mesh render mode: fill>";
+      break;
+
+    case 50:  // touche 2
+      renderer.mesh_render_mode = MeshRenderMode::wireframe;
+      ofLog() << "<mesh render mode: wireframe>";
+      break;
+
+    case 51:  // touche 3
+      renderer.mesh_render_mode = MeshRenderMode::vertex;
+      ofLog() << "<mesh render mode: vertex>";
+      break;
+
+    case OF_KEY_LEFT: // key ←
+      is_key_press_left = false;
+      break;
+
+    case OF_KEY_UP: // key ↑
+      is_key_press_up = false;
+      break;
+
+    case OF_KEY_RIGHT: // key →
+      is_key_press_right = false;
+      break;
+
+    case OF_KEY_DOWN: // key ↓
+      is_key_press_down = false;
+      break;
+
+    case 101: // key e
+      renderer.is_active_rotation = !renderer.is_active_rotation;
+      ofLog() << "<rotation is active: " << renderer.is_active_rotation << ">";
+      break;
+
+    case 102: // key f
+      renderer.is_flip_axis_y = !renderer.is_flip_axis_y;
+      ofLog() << "<axis Y is flipped: " << renderer.is_flip_axis_y << ">";
+      break;
+
+    case 114: // key r
+      renderer.is_active_proportion = !renderer.is_active_proportion;
+      ofLog() << "<proportion is active: " << renderer.is_active_proportion << ">";
+      break;
+
+    case 119: // key w
+      renderer.is_active_translation = !renderer.is_active_translation;
+      ofLog() << "<translation is active: " << renderer.is_active_translation << ">";
+      break;
+  }
+}
+
+void Application::exit()
+{
+  ofLog() << "<app::exit>";
+}
---
+++ module04/EX11/IFT3100H19_Teapot/src/renderer.h
@@ -0,0 +1,67 @@
+// IFT3100H19_Teapot/renderer.h
+// Classe responsable du rendu de l'application.
+
+#pragma once
+
+#include "ofMain.h"
+#include "ofxAssimpModelLoader.h"
+
+enum class MeshRenderMode{fill, wireframe, vertex};
+
+struct Locator
+{
+  float position[3];   // 3 * 4 = 12 octets
+  float rotation[3];   // 3 * 4 = 12 octets
+  float proportion[3]; // 3 * 4 = 12 octets
+};                     //       = 36 octets
+
+class Renderer
+{
+public:
+
+  Locator* locators;
+
+  ofxAssimpModelLoader teapot;
+
+  MeshRenderMode mesh_render_mode;
+
+  ofLight light;
+
+  ofNode node;
+
+  ofVec3f vector_position;
+  ofVec3f vector_rotation;
+  ofVec3f vector_proportion;
+
+  float center_x;
+  float center_y;
+
+  float offset_x;
+  float offset_z;
+
+  float delta_x;
+  float delta_z;
+
+  float speed;
+
+  int teapot_count;
+
+  int locator_buffer_head;
+
+  bool is_flip_axis_y;
+
+  bool is_active_translation;
+  bool is_active_rotation;
+  bool is_active_proportion;
+
+  void setup();
+  void update();
+  void reset();
+  void draw();
+
+  void dispatch_random_locator(int count, float range);
+
+  void draw_locator(float scale);
+
+  ~Renderer();
+};
