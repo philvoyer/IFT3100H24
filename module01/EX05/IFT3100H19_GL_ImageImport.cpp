8ba1e312b5d3ddde8175b2d87d184c2dc51110a6
philvoyer <philippe.voyer@ift.ulaval.ca>
2019-02-26 06:47:09 -0500

    publication exemple 1.5 (GL_ImageImport)

--- module01/EX05/IFT3100H19_GL_ImageImport.cpp
+++ module01/EX05/IFT3100H19_GL_ImageImport.cpp
@@ -55,8 +55,8 @@ glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
 // définir les paramètres d'interpolation des texels de la texture active
-glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
 // copier les pixels de l'image (mémoire RAM) vers la texture active (mémoire GPU)
 glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
