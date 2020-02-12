248b477f2ec2a6773753049ad3bab1526a48d4d0
philvoyer <philippe.voyer@ift.ulaval.ca>
2020-02-12 11:30:30 -0500

    publication exemple 4.6 (GL_VertexBufferSwap)

--- module04/EX06/IFT3100H20_GL_VertexBufferSwap.cpp
+++ module04/EX06/IFT3100H20_GL_VertexBufferSwap.cpp
@@ -1,4 +1,4 @@
-// IFT3100H19_GL_VertexBufferSwap.cpp
+// IFT3100H20_GL_VertexBufferSwap.cpp
 // Exemple d'une section de code qui inverse en alternance deux buffers de géométrie utilisés pour la mise à jour et le rendu afin d'éviter de bloquer le pipeline de rendu.
 
 // entrée
