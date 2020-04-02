1e139568546971dc5de2cdb528774aa890d867f6
philvoyer <philippe.voyer@ift.ulaval.ca>
2020-04-02 12:14:38 -0400

    publication exemple 9.1 (CurveBezier)

--- module09/EX01/IFT3100H20_CurveBezier/src/application.h
+++ module09/EX01/IFT3100H20_CurveBezier/src/application.h
@@ -1,4 +1,4 @@
-// IFT3100H19_CurveBezier/application.h
+// IFT3100H20_CurveBezier/application.h
 // Classe principale de l'application.
 
 #pragma once
--- module09/EX01/IFT3100H20_CurveBezier/src/renderer.h
+++ module09/EX01/IFT3100H20_CurveBezier/src/renderer.h
@@ -1,4 +1,4 @@
-// IFT3100H19_CurveBezier/renderer.h
+// IFT3100H20_CurveBezier/renderer.h
 // Classe responsable du rendu de l'application.
 
 #pragma once
