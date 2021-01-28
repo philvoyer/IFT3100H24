780ac757b3acb29ba3c769f6d22db681de8df29e
Philippe Voyer <philippe.voyer@ift.ulaval.ca>
2021-01-28 10:53:30 -0500

    publication exemple 1.2 (ColorSpaceGraymap)

--- module01/EX02/IFT3100H21_ColorSpaceGraymap/main.cpp
+++ module01/EX02/IFT3100H21_ColorSpaceGraymap/main.cpp
@@ -1,4 +1,4 @@
-// IFT3100H20_ColorSpaceGraymap\main.cpp
+// IFT3100H21_ColorSpaceGraymap\main.cpp
 // Génération procédurale d'une image dans un espace de couleur en niveau de gris (256 variations).
 // Les pixels sont colorés selon la valeur d'un xor entre ses coordonnées x et y.
 // Exportation des données de l'image dans un fichier .pgm (portable graymap).
