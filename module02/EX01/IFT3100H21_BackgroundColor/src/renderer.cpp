868b630d1c1a4a0ce45d61bd0de15e83643ed624
Philippe Voyer <philippe.voyer@ift.ulaval.ca>
2021-02-04 11:37:19 -0500

    publication exemple 2.1 (BackgroundColor)

--- module02/EX01/IFT3100H21_BackgroundColor/src/renderer.cpp
+++ module02/EX01/IFT3100H21_BackgroundColor/src/renderer.cpp
@@ -1,4 +1,4 @@
-// IFT3100H20_BackgroundColor/renderer.cpp
+// IFT3100H21_BackgroundColor/renderer.cpp
 // Classe responsable du rendu de l'application.
 
 #include "renderer.h"
--- module02/EX01/IFT3100H21_BackgroundColor/src/renderer.h
+++ module02/EX01/IFT3100H21_BackgroundColor/src/renderer.h
@@ -1,4 +1,4 @@
-// IFT3100H20_BackgroundColor/renderer.h
+// IFT3100H21_BackgroundColor/renderer.h
 // Classe responsable du rendu de l'application.
 
 #pragma once
