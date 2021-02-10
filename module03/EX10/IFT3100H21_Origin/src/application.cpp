43ac5940616d9bf94bbae76a4699e88b7e49ea50
Philippe Voyer <philippe.voyer@ift.ulaval.ca>
2021-02-10 14:55:16 -0500

    publication exemple 3.10 (Origin)

--- module03/EX10/IFT3100H21_Origin/src/application.cpp
+++ module03/EX10/IFT3100H21_Origin/src/application.cpp
@@ -1,4 +1,4 @@
-// IFT3100H20_Origin/application.cpp
+// IFT3100H21_Origin/application.cpp
 // Classe principale de l'application.
 
 #include "application.h"
--- module03/EX10/IFT3100H21_Origin/src/application.h
+++ module03/EX10/IFT3100H21_Origin/src/application.h
@@ -1,4 +1,4 @@
-// IFT3100H20_Origin/application.h
+// IFT3100H21_Origin/application.h
 // Classe principale de l'application.
 
 #pragma once
