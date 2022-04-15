7df347464e889a056914378b1731c0e6adb3828c
Philippe Voyer <philippe.voyer@ift.ulaval.ca>
2022-04-15 09:28:43 -0400

    publication exemple 10.1 (PBR)

--- module10/EX01/IFT3100H22_PBR/bin/data/shader/pbr_330_fs.glsl
+++ module10/EX01/IFT3100H22_PBR/bin/data/shader/pbr_330_fs.glsl
@@ -4,9 +4,9 @@
 
 #define PI 3.14159265358979323846264338327950288
 
-const float light_attenuation_factor_constant = 1.0;
-const float light_attenuation_factor_linear = 0.0014;
-const float light_attenuation_factor_quadratic = 0.000007;
+const float light_attenuation_factor_constant = 0.01;
+const float light_attenuation_factor_linear = 0.000001;
+const float light_attenuation_factor_quadratic = 0.0000001;
 
 // attributs de surface interpolés à partir des valeurs en sortie du shader de sommet
 in vec3 surface_position;
@@ -33,13 +33,13 @@ uniform float material_roughness;
 // facteur d'occlusion ambiante
 uniform float material_occlusion;
 
-// indice de réfraction de l'effet de fresnel;
+// indice de réfraction de l'effet de Fresnel;
 uniform vec3 material_fresnel_ior;
 
 // facteur d'exposition
 uniform float tone_mapping_exposure;
 
-// mode de mappage tonal (reinhard ou aces filmic)
+// mode de mappage tonal (Reinhard ou ACES filmic)
 uniform bool tone_mapping_toggle;
 
 // facteur gamma
@@ -66,7 +66,7 @@ uniform vec3 light_color;
 // intensité de la source de lumière
 uniform float light_intensity;
 
-// fonction de distribution des microfacettes
+// fonction de distribution des microfacettes (Trowbridge-Reitz)
 float trowbridge_reitz(vec3 n, vec3 h, float roughness)
 {
   float a = roughness * roughness;
@@ -79,7 +79,7 @@ float trowbridge_reitz(vec3 n, vec3 h, float roughness)
   return numerator / denominator;
 }
 
-// fonction géométrique pour calculer l'impact de l'occlusion et de l'ombrage des microfacettes
+// fonction géométrique pour calculer l'impact de l'occlusion et de l'ombrage des microfacettes (Schlick-Beckmann)
 float schlick_beckmann(float costheta, float roughness)
 {
   float r = (roughness + 1.0);
@@ -89,7 +89,7 @@ float schlick_beckmann(float costheta, float roughness)
   return numerator / denominator;
 }
 
-// fonction géométrique avec occlusion et ombrage combinés par la méthode de Smith
+// fonction géométrique avec occlusion et ombrage combinés (méthode de Smith)
 float smith(vec3 n, vec3 l, vec3 v, float roughness)
 {
   float ndl = max(dot(n, l), 0.0);
@@ -99,19 +99,19 @@ float smith(vec3 n, vec3 l, vec3 v, float roughness)
   return shadow * occlusion;
 }
 
-// fonction qui calcul l'effet de fresnel
+// fonction qui calcul l'effet de Fresnel
 vec3 schlick_fresnel(float costheta, vec3 f0)
 {
   return f0 + (1.0 - f0) * pow(1.0 - costheta, 5.0);
 }
 
-// mappage tonal de la couleur hdr vers ldr (reinhard tone mapping)
+// mappage tonal de la couleur HDR vers LDR (Reinhard tone mapping)
 vec3 tone_mapping_reinhard(vec3 x)
 {
   return x / (x + vec3(1.0));
 }
 
-// mappage tonal de la couleur hdr vers ldr (approximation de la courbe du ACES filmic tone mapping)
+// mappage tonal de la couleur HDR vers LDR (approximation de la courbe du ACES filmic tone mapping)
 vec3 tone_mapping_aces_filmic(vec3 x)
 {
   float a = 2.51f;
@@ -167,7 +167,7 @@ vec3 brdf_cook_torrance()
   float light_distance = length(light_position - surface_position);
 
   // calculer l'atténuation de l'intensité de la lumière en fonction de la distance
-  float light_attenuation = 1.0 / light_attenuation_factor_constant + light_attenuation_factor_linear * light_distance + light_attenuation_factor_quadratic * (light_distance * light_distance);
+  float light_attenuation = 1.0 / (light_attenuation_factor_constant + light_attenuation_factor_linear * light_distance + light_attenuation_factor_quadratic * (light_distance * light_distance));
 
   // calculer la radiance de la lumière
   vec3 radiance = light_color * light_attenuation * light_intensity;
@@ -184,10 +184,10 @@ vec3 brdf_cook_torrance()
   // reflexion de la surface avec un angle d'incidence nul
   vec3 f0 = material_fresnel_ior;
 
-  // moduler l'effet de fresnel ave la couleur diffuse en fonction du facteur de métallicité
+  // moduler l'effet de Fresnel ave la couleur diffuse en fonction du facteur de métallicité
   f0 = mix(f0, albedo, metallic);
 
-  // calculer l'effet de fresnel
+  // calculer l'effet de Fresnel
   vec3 f = schlick_fresnel(max(dot(h, v), 0.0), f0);
 
   // calculer le numérateur de l'équation (produit des fonctions d, f et g)
@@ -196,26 +196,26 @@ vec3 brdf_cook_torrance()
   // calculer le dénominateur de l'équation (facteur de normalisation)
   float coor_torrance_denominator = 4.0 * max(dot(n, v), 0.0) * diffuse_reflection;
 
-  // calculer la valeur de la fonction brdf de cook-torrance
+  // calculer le résultat de l'équation avec le numérateur et de dénominateur
   vec3 specular = coor_torrance_numerator / max(coor_torrance_denominator, 0.001);
 
   // mixer avec la couleur spéculaire du matériau
   specular = specular * material_color_specular;
 
-  // contribution spéculaire de la lumière
+  // calculer le ratio de réflection de la lumière à partir de l'effet de Fresnel (contribution spéculaire)
   vec3 ks = f;
 
-  // ratio de réfraction
+  // calculer le ratio de réfraction (contribution diffuse) proportionnelement à la contribution spéculaire
   vec3 kd = vec3(1.0) - ks;
 
-  // pondérer la réfraction selon le niveau de métallicité de la surface
+  // pondérer la contribution diffuse en fonction du niveau de métallicité de la surface
   kd *= 1.0 - metallic;
 
-  // calculer la réflectance
+  // calculer la réflectance de la fonction BRDF de Cook-Torrance
   vec3 reflectance = (kd * albedo / PI + specular) * radiance * diffuse_reflection;
 
   // mixer la couleur des composantes de réflexion
-  vec3 color = ambient + reflectance * material_brightness;
+  vec3 color = (ambient + reflectance) * material_brightness;
 
   // retourner la couleur
   return color;
@@ -229,7 +229,7 @@ void main()
   // ajustement de la couleur en fonction du facteur d'exposition
   color = vec3(1.0) - exp(-color * tone_mapping_exposure);
 
-  // mappage tonal de la couleur hdr vers ldr
+  // mappage tonal de la couleur HDR vers LDR
   if (tone_mapping_toggle)
     color = tone_mapping_aces_filmic(color);
   else
