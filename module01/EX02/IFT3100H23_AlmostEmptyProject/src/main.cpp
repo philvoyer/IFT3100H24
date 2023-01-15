8719a7899877d57d3a1cc55161f64c6286f77643
philvoyer <philvoyer@subsequent.dev>
2023-01-15 14:03:27 -0500

    publication exemple 1.2 (AlmostEmptyProject)

--- module01/EX02/IFT3100H23_AlmostEmptyProject/src/main.cpp
+++ module01/EX02/IFT3100H23_AlmostEmptyProject/src/main.cpp
@@ -13,6 +13,6 @@ int main()
   // création du contexte de rendu (OpenGL 2.1 par défaut)
   ofSetupOpenGL(512, 512, OF_WINDOW);
 
-  // instancier et lancer l'application (openFrameworks 0.11.0)
+  // instancier et lancer l'application (openFrameworks 0.11.2)
   ofRunApp(new Application());
 }
