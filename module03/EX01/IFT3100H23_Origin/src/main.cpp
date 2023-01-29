74320e391e2572bdf9e408e7865ca909eca977dd
philvoyer <philippe.voyer@ift.ulaval.ca>
2023-01-29 15:40:27 -0500

    publication exemple 3.1 (Origin)

---
+++ module03/EX01/IFT3100H23_Origin/src/main.cpp
@@ -0,0 +1,13 @@
+// IFT3100H23_Origin/main.cpp
+// Dans cet exemple, une représentation de l'origine de la scène est dessinée à deux localisations,
+// une avant transformation et une après transformation du système de coordonnées par translation
+// pour modifier la position de l'origine de la scène.
+
+#include "ofMain.h"
+#include "application.h"
+
+int main()
+{
+  ofSetupOpenGL(512, 512, OF_WINDOW);
+  ofRunApp(new Application());
+}
