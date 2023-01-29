8634eb977df7aa199d5df555e6e7fedfc6a04716
philvoyer <philippe.voyer@ift.ulaval.ca>
2023-01-29 15:44:39 -0500

    publication exemple 3.2 (Move)

---
+++ module03/EX02/IFT3100H23_Move/src/main.cpp
@@ -0,0 +1,12 @@
+// IFT3100H23_Move/main.cpp
+// Dans cet exemple, une transformation de type translation est appliquée sur l'origine de la scène en fonction du temps
+// pour permettre un mouvement dans l'espace sur le plan XZ à partir de l'état courant des flèches du clavier.
+
+#include "ofMain.h"
+#include "application.h"
+
+int main()
+{
+  ofSetupOpenGL(512, 512, OF_WINDOW);
+  ofRunApp(new Application());
+}
