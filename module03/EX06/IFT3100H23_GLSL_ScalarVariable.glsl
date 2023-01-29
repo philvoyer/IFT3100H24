c816d6ae09452b2e47c9871646e90aa748e4b09e
philvoyer <philippe.voyer@ift.ulaval.ca>
2023-01-29 15:51:34 -0500

    publication exemple 3.6 (GLSL_ScalarVariable)

---
+++ module03/EX06/IFT3100H23_GLSL_ScalarVariable.glsl
@@ -0,0 +1,35 @@
+// IFT3100H23_GLSL_ScalarVariable.glsl
+// Exemples de déclaration de variables scalaires en GLSL.
+
+// 1. constantes scalaires (nombres entiers)
+const int zero = 0;
+const int one = 1;
+
+// 2. constantes scalaires (nombres réels)
+const float two = 2.0;
+const float pi = 3.14159;
+const float phi = 1.61803;
+
+// 3. déclaration de variables de type booléen
+bool b1 = true;
+bool b2 = false;
+
+// 4. déclaration de variables scalaires de type nombre entier
+int i1 = 0;
+int i2 = 1;
+int i3 = -1;
+
+// 5. déclaration de variables scalaires de type nombre réel
+float f1 = 0.0;
+float f2 = 1.0;
+float f3 = -1.0;
+
+// 6. différentes options pour configurer le niveau de précision des variables numériques entières
+precision lowp int;
+precision mediump int;
+precision highp int;
+
+// 7. différentes options pour configurer le niveau de précision des variables numériques réelles
+precision lowp float;
+precision mediump float;
+precision highp float;
