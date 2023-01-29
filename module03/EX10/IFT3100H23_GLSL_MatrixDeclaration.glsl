a70ea8d5f5d02d54d2d400cad0b742493965fdc1
philvoyer <philippe.voyer@ift.ulaval.ca>
2023-01-29 16:01:44 -0500

    publication exemple 3.10 (MatrixDeclaration)

--- module03/EX10/IFT3100H23_GLSL_MatrixDeclaration.glsl
+++ module03/EX10/IFT3100H23_GLSL_MatrixDeclaration.glsl
@@ -7,8 +7,8 @@ mat3 ma33 = mat3(
   0.0, 1.0, 0.0,  // colonne 2
   0.0, 0.0, 1.0); // colonne 3
 
-// 2. matrice identité de dimension 3 par 3
-mat3 mb33(1.0);
+// 2. matrice identité de dimension 3 par 3 (forme compacte)
+mat3 mb33 = mat3(1.0);
 
 // 3. matrice identité de dimension 4 par 4
 mat4 ma44 = mat4(
@@ -17,8 +17,5 @@ mat4 ma44 = mat4(
   0.0, 0.0, 1.0, 0.0,  // colonne 3
   0.0, 0.0, 0.0, 1.0); // colonne 4
 
-// 4. matrice identité de dimension 4 par 4
-mat4 mb44(1.0);
-
-// 5. constantes de transformation (vecteur et matrice)
-mat4 identity = mat4(1.0);
+// 4. matrice identité de dimension 4 par 4 (forme compacte)
+mat4 mb44 = mat4(1.0);
