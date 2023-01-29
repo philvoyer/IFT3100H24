c8433b97930f3b155b46887922452cd3ce4082ea
philvoyer <philippe.voyer@ift.ulaval.ca>
2023-01-29 15:55:24 -0500

    publication exemple 3.10 (GLSL_MatrixDeclaration)

--- module03/EX10/IFT3100H23_GLSL_MatrixDeclaration.glsl
+++ module03/EX10/IFT3100H23_GLSL_MatrixDeclaration.glsl
@@ -1,4 +1,4 @@
-// IFT3100H23_MatrixDeclaration.glsl
+// IFT3100H23_GLSL_MatrixDeclaration.glsl
 // Exemples de déclaration de variables de type matrice en GLSL.
 
 // 1. matrice identité de dimension 3 par 3
@@ -19,3 +19,6 @@ mat4 ma44 = mat4(
 
 // 4. matrice identité de dimension 4 par 4
 mat4 mb44(1.0);
+
+// 5. constantes de transformation (vecteur et matrice)
+mat4 identity = mat4(1.0);
