061cbbc09d03af438566fb0645f4418fb37893fe
philvoyer <philvoyer@subsequent.dev>
2024-02-08 10:27:12 -0500

    publication exemple 3.10 (GLSL_MatrixDeclaration)

--- module03/EX10/IFT3100H24_GLSL_MatrixDeclaration.glsl
+++ module03/EX10/IFT3100H24_GLSL_MatrixDeclaration.glsl
@@ -1,4 +1,4 @@
-// IFT3100H23_GLSL_MatrixDeclaration.glsl
+// IFT3100H24_GLSL_MatrixDeclaration.glsl
 // Exemples de déclaration de variables de type matrice en GLSL.
 
 // 1. matrice identité de dimension 3 par 3
