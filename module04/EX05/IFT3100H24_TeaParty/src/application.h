8d1d929c5de29d22c60f553855d339f758c83334
philvoyer <philvoyer@subsequent.dev>
2024-02-27 11:20:48 -0500

    publication exemple 4.5 (TeaParty)

--- module04/EX05/IFT3100H24_TeaParty/src/application.h
+++ module04/EX05/IFT3100H24_TeaParty/src/application.h
@@ -1,4 +1,4 @@
-// IFT3100H23_TeaParty/application.h
+// IFT3100H24_TeaParty/application.h
 // Classe principale de l'application.
 
 #pragma once
--- module04/EX05/IFT3100H24_TeaParty/src/renderer.h
+++ module04/EX05/IFT3100H24_TeaParty/src/renderer.h
@@ -1,4 +1,4 @@
-// IFT3100H23_TeaParty/renderer.h
+// IFT3100H24_TeaParty/renderer.h
 // Classe responsable du rendu de l'application.
 
 #pragma once
