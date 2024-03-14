3e6e54f6aaa3b7da4d4093cf48b4d5c1aba5ad82
philvoyer <philvoyer@subsequent.dev>
2024-03-14 11:32:21 -0400

    publication exemple 6.3 (Convolution)

--- module06/EX03/IFT3100H24_Convolution/src/main.cpp
+++ module06/EX03/IFT3100H24_Convolution/src/main.cpp
@@ -1,4 +1,4 @@
-// IFT3100H23_Convolution/main.cpp
+// IFT3100H24_Convolution/main.cpp
 // Exemples d'application de filtres de convolution sur une image.
 
 #include "ofMain.h"
--- module06/EX03/IFT3100H24_Convolution/src/renderer.h
+++ module06/EX03/IFT3100H24_Convolution/src/renderer.h
@@ -1,4 +1,4 @@
-// IFT3100H23_Convolution/renderer.h
+// IFT3100H24_Convolution/renderer.h
 // Classe responsable du rendu de l'application.
 
 #pragma once
